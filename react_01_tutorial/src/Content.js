12882c573942fdf80ea8ab06e8af8b67c1713458
deepak thondapu <deepak.thondapu9@gmail.com>
2023-03-11 18:55:38 -0800

    react useState functionality to update in realtime

--- react_01_tutorial/src/Content.js
+++ react_01_tutorial/src/Content.js
@@ -1,14 +1,22 @@
 import React from 'react'
+import { useState } from 'react';
+
 
 const Content = () => {
+    const [name, setName] = useState('Deepak');
+    const [count, setCount] = useState(0);
+    // useState(0) implies the default value of count is 0
+    // Use them as const so that only the react state can change the values
+
     const handleNameChange = () => {
         const names = ['Bob',"Alen",'Kevin'];
         const int = Math.floor(Math.random() * 3);
-        return names[int];
+        setName(names[int]);
     }
 
-    const handleClick = () =>{
-        console.log('Clicked it');
+    const handleClick = (count) =>{
+        console.log(count+1);
+        setCount(count+1);
     }
 
     const handleClick2 = (name) =>{
@@ -21,12 +29,11 @@ const Content = () => {
     return (
         <main>
             <p onClick={handleClick}>
-                Hello {handleNameChange()}!
-
+                Hello {name}!
             </p>
 
-            <button onClick={handleClick}>Click it</button>
-            <button onClick={() => handleClick2('Deepak')}>Click it</button>
+            <button onClick={handleNameChange}>Change name</button>
+            <button onClick={() => handleClick(count)}>Click it</button>
             <button onClick={(e) => handleClick3(e)}>Click it</button>
         </main>
     )
